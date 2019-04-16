cd5e99763e98e68f7a255ce06be5331ccc88000c
joyoverflow <joyoverflow@sina.com>
2019-04-16 23:47:40 +0800

    添加背景图处理

--- CircularImage/src/main/java/pxgd/hyena/com/circularimage/MainActivity.java
+++ CircularImage/src/main/java/pxgd/hyena/com/circularimage/MainActivity.java
@@ -1,16 +1,53 @@
 package pxgd.hyena.com.circularimage;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //https://blog.csdn.net/songzi1228/article/details/78962327
+        //https://blog.csdn.net/ainiyiwan123/article/details/79788888
+        RelativeLayout r_layout = findViewById(R.id.r_layout);
+        //r_layout.setBackground(R.drawable.women);
+        //r_layout.setImageBitmap(BitmapFactory.decodeFile(photoPath));
+
+        //E:\AndoridWork\ImageView
+        //悬浮按钮
+        //https://github.com/makovkastar/FloatingActionButton
+        //https://github.com/Clans/FloatingActionButton
+        //https://www.jianshu.com/p/9b0304b17f26
+        //https://blog.csdn.net/gaolh89/article/details/79759404
+
+
+        img = findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.mountain);
+        img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        /**
+         * app:fabSize="mini" FAB大小，normal=56dp或mini=40dp
+         */
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Click",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
 
         /*
         CircularImageButton colorChanger = findViewById(R.id.circleButton);
@@ -27,17 +64,54 @@ public class MainActivity extends AppCompatActivity {
         */
 
 
-        VImageButtonView imageButtonView = findViewById(R.id.pic1);
-        imageButtonView.setOnClickListener(new View.OnClickListener() {
+        VImageButtonView btnImg1 = findViewById(R.id.pic1);
+        btnImg1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
                 Toast.makeText(
                         MainActivity.this,
                         "Click",
                         Toast.LENGTH_SHORT
                 ).show();
+                */
+                img.setImageResource(R.drawable.women);
+            }
+        });
+        VImageButtonView btnImg2 = findViewById(R.id.pic2);
+        btnImg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Toast.makeText(
+                        MainActivity.this,
+                        "Click",
+                        Toast.LENGTH_SHORT
+                ).show();
+                */
+                img.setImageResource(R.drawable.elves);
             }
         });
 
+    }
+
+    /**
+     * 沉浸模式（界面全屏，顶部状态栏和底部导航栏都不会显示）
+     * @param hasFocus
+     * */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
     }
 }
