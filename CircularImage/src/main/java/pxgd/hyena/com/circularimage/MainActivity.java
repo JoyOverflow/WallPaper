0b878fec64471cf8ca53761fa661f22367bb5f60
joyoverflow <joyoverflow@sina.com>
2019-04-17 23:22:22 +0800

    修改背景缩略图样式

--- CircularImage/src/main/java/pxgd/hyena/com/circularimage/MainActivity.java
+++ CircularImage/src/main/java/pxgd/hyena/com/circularimage/MainActivity.java
@@ -1,5 +1,6 @@
 package pxgd.hyena.com.circularimage;
 
+import android.app.WallpaperManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,9 +10,13 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView img;
+    private int resId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +35,16 @@ public class MainActivity extends AppCompatActivity {
         //https://www.jianshu.com/p/9b0304b17f26
         //https://blog.csdn.net/gaolh89/article/details/79759404
 
-
+        resId=R.drawable.mountain;
         img = findViewById(R.id.imageView);
         img.setImageResource(R.drawable.mountain);
+
+        //大于ImageView的图像被缩放到填充整个ImageView并居中显示
+        //小于ImageView的图像会被拉伸到填充整个ImageView
+        //非ImageView宽高比例的图像会裁剪掉多余部分（ImageView内不会有多余宽或高）
         img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+
         /**
          * app:fabSize="mini" FAB大小，normal=56dp或mini=40dp
          */
@@ -41,9 +52,18 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //设置手机的壁纸
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(MainActivity.this);
+                try {
+                    wallpaperManager.setResource(resId);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 Toast.makeText(
                         MainActivity.this,
-                        "Click",
+                        "手机壁纸已更改！",
                         Toast.LENGTH_SHORT
                 ).show();
             }
@@ -75,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT
                 ).show();
                 */
+                resId=R.drawable.women;
                 img.setImageResource(R.drawable.women);
             }
         });
@@ -89,9 +110,58 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT
                 ).show();
                 */
+                resId=R.drawable.elves;
                 img.setImageResource(R.drawable.elves);
             }
         });
+        VImageButtonView btnImg3 = findViewById(R.id.pic3);
+        btnImg3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Toast.makeText(
+                        MainActivity.this,
+                        "Click",
+                        Toast.LENGTH_SHORT
+                ).show();
+                */
+                resId=R.drawable.centaur;
+                img.setImageResource(R.drawable.centaur);
+            }
+        });
+        VImageButtonView btnImg4 = findViewById(R.id.pic4);
+        btnImg4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Toast.makeText(
+                        MainActivity.this,
+                        "Click",
+                        Toast.LENGTH_SHORT
+                ).show();
+                */
+                resId=R.drawable.car_house;
+                img.setImageResource(R.drawable.car_house);
+            }
+        });
+        VImageButtonView btnImg5 = findViewById(R.id.pic5);
+        btnImg5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Toast.makeText(
+                        MainActivity.this,
+                        "Click",
+                        Toast.LENGTH_SHORT
+                ).show();
+                */
+                resId=R.drawable.mountain;
+                img.setImageResource(R.drawable.mountain);
+            }
+        });
+
+
+
 
     }
 
