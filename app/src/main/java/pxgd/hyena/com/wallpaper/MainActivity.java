b89382007c5a7833820fcd07e4d9f06b86b5853a
joyoverflow <joyoverflow@sina.com>
2019-04-19 23:54:48 +0800

    完成抽屉布局

--- app/src/main/java/pxgd/hyena/com/wallpaper/MainActivity.java
+++ app/src/main/java/pxgd/hyena/com/wallpaper/MainActivity.java
@@ -1,13 +1,42 @@
 package pxgd.hyena.com.wallpaper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //DrawerLayout.fitsSystemWindows="true" 设置根布局延伸到状态栏
+
+        //NavigationView（header和menu布局）
+        //必需填加依赖'com.android.support:design:26.1.0'
+
+        //使用ToolBar替换掉以前的ActionBar（清单Manifest文件中@style/AppTheme样式已更新，否则会异常）
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+
+        /*
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        */
+
     }
 }
