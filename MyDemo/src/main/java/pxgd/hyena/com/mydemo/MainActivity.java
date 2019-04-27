96a2e6d1b63f4f7ee825a8784e4dd68a5d5fb1aa
joyoverflow <joyoverflow@sina.com>
2019-04-27 23:55:33 +0800

    保存图片

--- MyDemo/src/main/java/pxgd/hyena/com/mydemo/MainActivity.java
+++ MyDemo/src/main/java/pxgd/hyena/com/mydemo/MainActivity.java
@@ -1,12 +1,18 @@
 package pxgd.hyena.com.mydemo;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements
-        View.OnClickListener,View.OnLongClickListener{
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity{
 
     public static final String TAG = "MainActivity";
 
@@ -14,19 +20,85 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.btnTest).setOnClickListener(this);
-        findViewById(R.id.btnTest).setOnLongClickListener(this);
     }
     @Override
-    public void onClick(View v) {
-        Log.d(TAG, "onClick！");
-    }
-    @Override
-    public boolean onLongClick(View v) {
-        Log.d(TAG, "nLongClick！");
-
-        //true为仅长按操作（事件已消耗），不加短点击事件
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //DIRECTORY_DCIM 相机拍摄的图片和视频保存位置
+        //DIRECTORY_PICTURES 下载图片保存的位置
+        //File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File dir = Environment.getDownloadCacheDirectory();
+
+
+
+        //File dir = getFilesDir();
+        File file = new File(dir, "/104259.jpg");
+        switch (item.getItemId()) {
+            case R.id.action_share1:
+                Log.d(TAG, file.toURI().toString());
+                shareToFriend(file);
+                return true;
+            case R.id.action_share2:
+                Log.d(TAG, file.toURI().toString());
+                shareToTimeLine(file);
+                return true;
+            case R.id.action_share3:
+                File video = new File(dir, "/test.mp4");
+                shareToFriendVideo(video);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void shareToFriend(File file) {
+        Intent intent = new Intent();
+        ComponentName comp = new ComponentName("com.tencent.mm",
+                "com.tencent.mm.ui.tools.ShareImgUI");
+        intent.setComponent(comp);
+        intent.setAction("android.intent.action.SEND");
+        intent.setType("image/*");
+        //intent.setFlags(0x3000001);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+        startActivity(intent);
+    }
+    private void shareToFriendVideo(File file) {
+
+        Intent intent = new Intent();
+        ComponentName comp = new ComponentName("com.tencent.mm",
+                "com.tencent.mm.ui.tools.ShareImgUI");
+        intent.setComponent(comp);
+        intent.setAction("android.intent.action.SEND");
+        intent.setType("video/*");
+        //intent.setFlags(0x3000001);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+        startActivity(intent);
+    }
+    private void shareToTimeLine(File file) {
+        Intent intent = new Intent();
+        ComponentName comp = new ComponentName("com.tencent.mm",
+                "com.tencent.mm.ui.tools.ShareToTimeLineUI");
+        intent.setComponent(comp);
+        intent.setAction("android.intent.action.SEND");
+        intent.setType("image/*");
+        //intent.setFlags(0x3000001);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+        startActivity(intent);
+    }
+
+
+
+
+
+
+
+
+
+
 }
