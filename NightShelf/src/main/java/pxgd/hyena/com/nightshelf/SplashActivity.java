9d910eeb526569772f9fe0ab4b6c58c8429637b1
joyoverflow <joyoverflow@sina.com>
2019-05-03 23:02:05 +0800

    添加引导页和图片资源

--- NightShelf/src/main/java/pxgd/hyena/com/nightshelf/SplashActivity.java
+++ NightShelf/src/main/java/pxgd/hyena/com/nightshelf/SplashActivity.java
@@ -1,7 +1,10 @@
 package pxgd.hyena.com.nightshelf;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, 2000);
     }
 }
