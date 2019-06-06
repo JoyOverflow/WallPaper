5847f665118dd029854b96282258f9eb232db88f
joyoverflow <joyoverflow@sina.com>
2019-06-06 23:57:45 +0800

    添加模块TreadBook

---
+++ TreadBook/src/main/java/ouyj/hyena/com/treadbook/MainActivity.java
@@ -0,0 +1,13 @@
+package ouyj.hyena.com.treadbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
