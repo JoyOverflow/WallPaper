14106a610851921f712bc1caa01a13e7db61ad03
joyoverflow <joyoverflow@sina.com>
2019-06-08 21:26:12 +0800

    修改引导活动

--- TreadBook/src/main/java/ouyj/hyena/com/treadbook/GuideActivity.java
+++ TreadBook/src/main/java/ouyj/hyena/com/treadbook/GuideActivity.java
@@ -1,10 +1,12 @@
 package ouyj.hyena.com.treadbook;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
+/**
+ * 引导活动
+ */
 public class GuideActivity extends AppCompatActivity {
 
     @Override
@@ -12,10 +14,13 @@ public class GuideActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
 
-        initData();
+        //延时并转向另一活动
+        redirectActivity();
     }
-
-    protected void initData() {
+    /**
+     * 重定位活动
+     */
+    protected void redirectActivity() {
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
@@ -23,7 +28,10 @@ public class GuideActivity extends AppCompatActivity {
         getWindow().getDecorView().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(GuideActivity.this,MainActivity.class);
+                Intent intent = new Intent(
+                        GuideActivity.this,
+                        MainActivity.class
+                );
                 GuideActivity.this.startActivity(intent);
                 GuideActivity.this.finish();
             }
